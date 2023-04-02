36f8a8f685dcc2e01688caa31d348ef6ad5970a2
bogdan-kovalchuk <35914116+bogdan-kovalchuk@users.noreply.github.com>
2023-04-02 16:00:00 +0300

    Add image file validation to validation module

--- validation.py
+++ validation.py
@@ -5,6 +5,9 @@ import os
 
 REQUIRED_DESCRIPTION_KEYS = ["name", "weight", "description", "image_name"]
 
+ALLOWED_IMAGE_EXTENSIONS = {".jpeg", ".jpg", ".tiff", ".tif", ".png"}
+MAX_IMAGE_SIZE_BYTES = 50 * 1024 * 1024
+
 
 def validate_description_file(path):
     errors = []
@@ -43,4 +46,38 @@ def validate_directory(path, label="directory"):
         errors.append("{} path is empty".format(label))
     elif not os.path.isdir(path):
         errors.append("{} does not exist: {}".format(label, path))
+    return errors
+
+
+def validate_image_file(path, max_size_bytes=MAX_IMAGE_SIZE_BYTES):
+    errors = []
+    if not path or not isinstance(path, str):
+        errors.append("Image path is empty")
+        return errors
+    if not os.path.isfile(path):
+        errors.append("Image file not found: {}".format(path))
+        return errors
+    _, ext = os.path.splitext(path)
+    if ext.lower() not in ALLOWED_IMAGE_EXTENSIONS:
+        errors.append(
+            "Unsupported image format {}: {}".format(ext, path)
+        )
+    size = os.path.getsize(path)
+    if size == 0:
+        errors.append("Image file is empty: {}".format(path))
+    elif size > max_size_bytes:
+        errors.append(
+            "Image exceeds max size ({}B > {}B): {}".format(
+                size, max_size_bytes, path
+            )
+        )
+    return errors
+
+
+def validate_image_dimensions(width, height):
+    errors = []
+    if not isinstance(width, int) or width <= 0:
+        errors.append("Width must be a positive integer, got {}".format(width))
+    if not isinstance(height, int) or height <= 0:
+        errors.append("Height must be a positive integer, got {}".format(height))
     return errors
