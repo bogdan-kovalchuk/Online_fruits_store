33568fbcc09adaf715f17c3ebbbf0605b5680a08
bogdan-kovalchuk <35914116+bogdan-kovalchuk@users.noreply.github.com>
2023-06-29 15:00:00 +0300

    Add SMTP timeout and failure handling to emails.py

--- emails.py
+++ emails.py
@@ -5,38 +5,41 @@ import mimetypes
 import os.path
 import smtplib
 
-def generate(sender, recipient, subject, body, attachment_path):
-  """Creates an email with an attachement."""
-  # Basic Email formatting
-  message = email.message.EmailMessage()
-  message["From"] = sender
-  message["To"] = recipient
-  message["Subject"] = subject
-  message.set_content(body)
+SMTP_TIMEOUT = 30
 
-  # Process the attachment and add it to the email
-  attachment_filename = os.path.basename(attachment_path)
-  mime_type, _ = mimetypes.guess_type(attachment_path)
-  mime_type, mime_subtype = mime_type.split('/', 1)
 
-  with open(attachment_path, 'rb') as ap:
-    message.add_attachment(ap.read(),
-                          maintype=mime_type,
-                          subtype=mime_subtype,
-                          filename=attachment_filename)
+def generate(sender, recipient, subject, body, attachment_path=None):
+    message = email.message.EmailMessage()
+    message["From"] = sender
+    message["To"] = recipient
+    message["Subject"] = subject
+    message.set_content(body)
 
-  return message
+    if attachment_path is not None:
+        attachment_filename = os.path.basename(attachment_path)
+        mime_type, _ = mimetypes.guess_type(attachment_path)
+        if mime_type is None:
+            mime_type = "application/octet-stream"
+        mime_type, mime_subtype = mime_type.split('/', 1)
 
-def generate_error_report(sender, recipient, subject, body):
-  message = email.message.EmailMessage()
-  message["From"] = sender
-  message["To"] = recipient
-  message["Subject"] = subject
-  message.set_content(body)
-  return message
+        with open(attachment_path, 'rb') as ap:
+            message.add_attachment(ap.read(),
+                                  maintype=mime_type,
+                                  subtype=mime_subtype,
+                                  filename=attachment_filename)
 
-def send(message):
-  """Sends the message to the configured SMTP server."""
-  mail_server = smtplib.SMTP('localhost')
-  mail_server.send_message(message)
-  mail_server.quit()
+    return message
+
+
+def generate_error_report(sender, recipient, subject, body, attachment_path=None):
+    return generate(sender, recipient, subject, body, attachment_path)
+
+
+def send(message, smtp_host='localhost', timeout=SMTP_TIMEOUT):
+    try:
+        mail_server = smtplib.SMTP(smtp_host, timeout=timeout)
+        mail_server.send_message(message)
+        mail_server.quit()
+        return True
+    except (smtplib.SMTPException, OSError):
+        return False
