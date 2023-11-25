9342fa065b1702aef0755319b8f5eca463dcf7ba
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2023-11-25 12:00:00 +0300

    Add dry-run to image scripts

--- changeImage.py
+++ changeImage.py
@@ -1,14 +1,54 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
+import sys
 from PIL import Image
+import config
+import validation
 
-user = os.getenv('USER')
-images_path = '/home/{}/supplier-data/images/'.format(user)
-for image in os.listdir(images_path):
-    if image.endswith('tiff'):
-        image_path = os.path.join(images_path, image)
-        image_path_without_ext = os.path.splitext(image_path)[0]
-        out_image_path = '{}.jpeg'.format(image_path_without_ext)
-        image = Image.open(image_path)
-        image.convert("RGB").resize((600, 400)).save(out_image_path, "JPEG")
+
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser(description="Convert TIFF images to JPEG.")
+    parser.add_argument("--dry-run", action="store_true",
+                        help="Print actions without converting images")
+    return parser.parse_args(argv)
+
+
+def convert_images(images_path, dry_run=False):
+    results = []
+    for image_name in os.listdir(images_path):
+        if image_name.endswith('tiff'):
+            image_path = os.path.join(images_path, image_name)
+            image_path_without_ext = os.path.splitext(image_path)[0]
+            out_image_path = '{}.jpeg'.format(image_path_without_ext)
+            if dry_run:
+                results.append(("dry-run", image_name, out_image_path))
+            else:
+                img = Image.open(image_path)
+                img.convert("RGB").resize(
+                    (config.DEFAULT_IMAGE_WIDTH, config.DEFAULT_IMAGE_HEIGHT)
+                ).save(out_image_path, "JPEG")
+                results.append(("converted", image_name, out_image_path))
+    return results
+
+
+def main(argv=None):
+    args = parse_args(argv)
+    user = config.get_user()
+    images_path = config.get_images_path(user)
+    dir_errors = validation.validate_directory(images_path, "Images path")
+    if dir_errors:
+        for e in dir_errors:
+            print("Error: {}".format(e), file=sys.stderr)
+        sys.exit(1)
+    results = convert_images(images_path, dry_run=args.dry_run)
+    for action, name, detail in results:
+        if action == "dry-run":
+            print("Would convert: {} -> {}".format(name, detail))
+        else:
+            print("Converted {} -> {}".format(name, detail))
+
+
+if __name__ == "__main__":
+    main()
--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -1,15 +1,51 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
+import sys
 import requests
+import config
+import validation
 
-url = "http://localhost/upload/"
-user = os.getenv('USER')
-images_path = '/home/{}/supplier-data/images/'.format(user)
-for image in os.listdir(images_path):
-    if image.endswith('jpeg'):
-        image_path = os.path.join(images_path, image)
-        image_path_without_ext = os.path.splitext(image_path)[0]
-        out_image_path = '{}.jpeg'.format(image_path_without_ext)
-        with open(image_path, 'rb') as opened:
-            r = requests.post(url, files={'file': opened})
+
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser(description="Upload JPEG images to server.")
+    parser.add_argument("--dry-run", action="store_true",
+                        help="Print actions without uploading")
+    return parser.parse_args(argv)
+
+
+def upload_images(images_path, url, dry_run=False):
+    results = []
+    for image_name in os.listdir(images_path):
+        if image_name.endswith('jpeg'):
+            image_path = os.path.join(images_path, image_name)
+            if dry_run:
+                results.append(("dry-run", image_name, url))
+            else:
+                with open(image_path, 'rb') as opened:
+                    r = requests.post(url, files={'file': opened})
+                results.append(("uploaded", image_name, r.status_code))
+    return results
+
+
+def main(argv=None):
+    args = parse_args(argv)
+    url = config.DEFAULT_UPLOAD_URL
+    user = config.get_user()
+    images_path = config.get_images_path(user)
+    dir_errors = validation.validate_directory(images_path, "Images path")
+    if dir_errors:
+        for e in dir_errors:
+            print("Error: {}".format(e), file=sys.stderr)
+        sys.exit(1)
+    results = upload_images(images_path, url, dry_run=args.dry_run)
+    for action, name, detail in results:
+        if action == "dry-run":
+            print("Would upload: {} -> {}".format(name, detail))
+        else:
+            print("Uploaded {} (status {})".format(name, detail))
+
+
+if __name__ == "__main__":
+    main()
